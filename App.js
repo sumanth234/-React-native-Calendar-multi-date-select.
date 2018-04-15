176c0571d98feab62a584b0c355cf54530e17b19
usumanth <sumanth.u@compassitesinc.com>
2018-04-16 02:08:09 +0530

    Non Debug Mode Issue Fixed

--- App.js
+++ App.js
@@ -11,93 +11,60 @@ import {
   Image
 } from 'react-native';
 import Modal from 'react-native-modal';
-import { CalendarList } from 'react-native-calendars'
+import { CalendarList } from 'react-native-calendars';
+import moment from 'moment'
 const deviceWidth = Dimensions.get('window').width
 export default class App extends Component{
+
   constructor(props){
     super(props);
-    if(new Date().getHours() >= 0 && new Date().getHours() < 4){
-        start = new Date(new Date().getFullYear() + ", " + (new Date().getMonth()+1) + ", " + ((new Date().getDate())-1))
-        end = new Date()
+    if(moment().format('HH') >= 0 && moment().format('HH') < 4){
+      start = moment().subtract(1, 'days').format('YYYY-MM-DD')
+      end = moment().format('YYYY-MM-DD')
     }else{
-        start = new Date()
-        end = new Date(new Date().getFullYear() + ", " + (new Date().getMonth()+1) + ", " + ((new Date().getDate())+1))
+        start = moment().format('YYYY-MM-DD')
+        end = moment().add(1,'days').format('YYYY-MM-DD')
     }
-    this.state =({
-      date:new Date(),
+    this.state = ({
+      date:moment(),
       showModal:false,
-      year:new Date().getFullYear(),
-      month:new Date().getMonth() + 1,
+      year:moment().year(),
+      month:moment().month(),
       pastScroll:0,
-      currentMonth:new Date().getMonth() + 1,
-      currentYear:new Date().getFullYear(),
+      currentMonth:moment().month(),
+      currentYear:moment().year(),
       dateDuration:"",
       monthInName:"",
-      maxDate: new Date().setFullYear(new Date().getFullYear() + 1),
+      maxDate: moment().add(1,'years'),
       selectDate:"StartDate",
       startDate:start,
-      endDate: end
+      endDate:end
     })
   }
+
   onMonthChange(val){
     
     if(val.length == 1){
-      console.log(val[0].year - this.state.currentYear,"year")
-      
-      console.log(val[0].month - (this.state.currentMonth + 1),"month")
       this.setState({month:val[0].month,year:val[0].year})
     }
   }
+  
   render() {
-    var pastScroll,sd,ed,min ;    
-    var options = {  
-      weekday: "short", month: "short", day: "numeric"  
-    };  
-    var options1 = {
-      day:"numeric",month: "short",year:"numeric"
-    };
+    var pastScroll,sd,ed,min ;
     if(this.state.year - this.state.currentYear  == 1){
       pastScroll=(12-(this.state.currentMonth - this.state.month))
     }else{
       pastScroll=(this.state.month - this.state.currentMonth)
     }
-    if(this.state.startDate.getDate() <10){
-      if(this.state.startDate.getMonth() < 10){
-        sd = + this.state.startDate.getFullYear() + "-0" + (this.state.startDate.getMonth()+1) + "-0" +this.state.startDate.getDate()
-      }else{
-        sd = this.state.startDate.getFullYear() + "-" + (this.state.startDate.getMonth()+1) + "-0" +this.state.startDate.getDate()
-      }
-    }else{
-      if(this.state.startDate.getMonth() < 10){
-        sd = + this.state.startDate.getFullYear() + "-0" + (this.state.startDate.getMonth()+1) + "-" +this.state.startDate.getDate()
-      }else{
-        sd = this.state.startDate.getFullYear() + "-" + (this.state.startDate.getMonth()+1) + "-" +this.state.startDate.getDate()
-      }
-    }
-    if(this.state.endDate.getDate() < 10){
-      if(this.state.endDate.getMonth() < 10){
-        ed = this.state.endDate.getFullYear() + "-0" + (this.state.endDate.getMonth()+1) + "-0" +this.state.endDate.getDate() 
-        }else{
-          ed = this.state.endDate.getFullYear() + "-" + (this.state.endDate.getMonth()+1) + "-0" +this.state.endDate.getDate() 
-        }
-    }else{
-      if(this.state.endDate.getMonth() < 10){
-        ed = this.state.endDate.getFullYear() + "-0" + (this.state.endDate.getMonth()+1) + "-" +this.state.endDate.getDate()
-        }else{
-        ed = this.state.endDate.getFullYear() + "-" + (this.state.endDate.getMonth()+1) + "-" +this.state.endDate.getDate() 
-        }
-    }
     if(this.state.selectDate === "StartDate"){
-      if(this.state.date.getHours() >= 0 && this.state.date.getHours() < 4){
-        min = new Date(new Date().getFullYear() + ", " + (new Date().getMonth()+1) + ", " + ((new Date().getDate())-1))
+      if(moment(this.state.date).format('HH') >= 0 && moment(this.state.date).format('HH') < 4){
+        min = moment().subtract(1, 'days').format('YYYY-MM-DD')
       }else{
-        min = this.state.date
+        min = moment(this.state.date).format('YYYY-MM-DD')
       }
     }else{
-      min = this.state.startDate
+      min = moment(this.state.startDate).format('YYYY-MM-DD')
     }
-    console.log("sd",sd)
-    console.log("ed",ed)
     return (
       <View style={styles.container}>
         <View style={{justifyContent:'flex-start',alignContent:'center',borderRadius:12,borderWidth:1.2,width:330,borderColor:'grey',flexDirection:'row'}}>
@@ -111,7 +78,7 @@ export default class App extends Component{
             <View style={{width:330,height:60,flexDirection:'row',justifyContent:'center',alignItems:'center',backgroundColor:'#00001a'}}>
               <TouchableOpacity style={{flexDirection:'column',marginTop:8}} onPress={() => this.setState({selectDate:"StartDate"})}>
                 {this.state.selectDate === "StartDate"? <Text style={{textAlign:'center',width:110,color:'white',fontWeight:'bold'}}>Check In</Text>: <Text style={{textAlign:'center',width:110,color:'white'}}>Check In</Text>}
-                <Text style={{textAlign:'center',width:110,color:'white',fontWeight:'bold'}}>{this.state.startDate.toLocaleDateString("en-us", options)}</Text>
+                <Text style={{textAlign:'center',width:110,color:'white',fontWeight:'bold'}}>{moment(this.state.startDate).format('ddd, DD MMM')}</Text>
                 {this.state.selectDate === "StartDate" ?<Image source={require('./arrow.png')} style={{width:20,height:7,alignSelf:'center',marginTop:7}} />:<View />}
               </TouchableOpacity>
               <View style={{width:110,justifyContent:'center',alignItems:'center'}}>
@@ -119,28 +86,31 @@ export default class App extends Component{
               </View>
               <TouchableOpacity style={{flexDirection:'column',marginTop:8}} onPress={() => this.setState({selectDate:"EndDate"})}>
                 {this.state.selectDate === "EndDate"?<Text style={{textAlign:'center',color:'white',fontWeight:'bold',width:110}}>Check Out</Text>:<Text style={{textAlign:'center',width:110,color:'white'}}>Check Out</Text>}
-                <Text style={{textAlign:'center',width:110,color:'white',fontWeight:'bold'}}>{this.state.endDate.toLocaleDateString("en-us", options)}</Text>
+                <Text style={{textAlign:'center',width:110,color:'white',fontWeight:'bold'}}>{moment(this.state.endDate).format('ddd, DD MMM')}</Text>
                 {this.state.selectDate === "EndDate" ?<Image source={require('./arrow.png')} style={{width:20,height:7,alignSelf:'center',marginTop:7}} />:<View />}
               </TouchableOpacity>
             </View>
             <CalendarList
+              //ref={(c) => this.calendar = c}
               horizontal={true}
               pagingEnabled={true}
-              current= {this.state.startDate}
-              minDate={min}
-              maxDate={this.state.maxDate}
+              current= {moment(this.state.startDate).format('YYYY-MM-DD')}
+              minDate= {min}
+              maxDate={moment(this.state.maxDate).format('YYYY-MM-DD')}
               onDayPress={(day) => {
+                console.log("day",moment(day.dateString).format('MMMM'))
+                //this.calendar.scrollToMonth("'" + moment(day.dateString).format('MMMM') + "'")
                 if(this.state.selectDate ==="StartDate"){
-                  this.setState({selectDate:"EndDate",startDate:(new Date(("'" + day.dateString + "'"))),
-                  endDate: new Date(new Date(("'" + day.dateString + "'")).getFullYear() + ", " + (new Date(("'" + day.dateString + "'")).getMonth()+1) + ", " + ((new Date(("'" + day.dateString + "'")).getDate())+1))
-                })
+                  this.setState({selectDate:"EndDate",startDate:(moment(day.dateString)),
+                  endDate: moment(day.dateString).add(1,'days')})
                 }else if(this.state.selectDate ==="EndDate"){
-                  this.setState({endDate:(new Date(("'" + day.dateString + "'")))})}
+                  this.setState({endDate:moment(day.dateString)})
                 }
-              }
+                
+              }}
               hideArrows={true}
               hideExtraDays={false}
-              disableMonthChange={true}
+              disableMonthChange={false}
               onVisibleMonthsChange={(months) => this.onMonthChange.bind(this)(months)}
               firstDay={0}
               hideDayNames={false}
@@ -149,8 +119,8 @@ export default class App extends Component{
               pastScrollRange={pastScroll}
               futureScrollRange={12-pastScroll}
               markedDates={{
-                [sd] : {disabled: true, startingDay: true, color: '#e65c00', endingDay: true},
-                [ed] : {disabled: true, startingDay: true, color: '#e65c00', endingDay: true},
+                [moment(this.state.startDate).format('YYYY-MM-DD')] : {disabled: true,marked:true,selected:true, startingDay: true, color: '#e65c00', endingDay: true},
+                [moment(this.state.endDate).format('YYYY-MM-DD')] : {disabled: true, startingDay: true, color: '#e65c00', endingDay: true},
               }}
               markingType={'period'}
             />
@@ -158,7 +128,7 @@ export default class App extends Component{
               <TouchableOpacity onPress={() => this.setState({showModal:false})}>
                 <Text style={{textAlign:'right',color:'grey',padding:15,fontSize:18}} >Cancel</Text>
               </TouchableOpacity>
-              <TouchableOpacity onPress={() => this.setState({showModal:false,dateDuration:(this.state.startDate.toLocaleDateString("en-us", options1) + " - " + this.state.endDate.toLocaleDateString("en-us", options1))})}>
+              <TouchableOpacity onPress={() => this.setState({showModal:false,dateDuration:(moment(this.state.startDate).format('DD MMM YYYY') + " - " + moment(this.state.endDate).format('DD MMM YYYY'))})}>
                 <Text style={{textAlign:'right',color:'black',padding:15,marginRight:10,fontSize:18}} >Done</Text>
               </TouchableOpacity>
             </View>         
